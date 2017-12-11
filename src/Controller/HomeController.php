1f5fc5acd00e16bda720bda2ba6d5409e303b86e
Andrusik <gigiandrusik@i.ua>
2017-12-12 00:03:58 +0200

    add front

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function indexAction()
+    {
+        return $this->render('site/index.html.twig');
+    }
+}
